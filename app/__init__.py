538132a08a29f86d1475b050674713e0b1fadb51
Richard Ma <richard.ma.19850509@gmail.com>
2023-05-25 14:48:38 +0800

    fix #4

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass # instance folder exists
 
     @app.route("/")
     def index():
