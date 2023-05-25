6e1dd10e77b2055cc415b06ef8724f4136f4fce0
Richard Ma <richard.ma.19850509@gmail.com>
2023-05-25 23:22:16 +0800

    fix #11

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from werkzeug.utils import import_string
 
@@ -18,10 +20,28 @@ def create_app():
     # load configration
     if e == 'dev':
         cfg_name = 'config.DevelopmentConfig'
+        log_filename = 'development.log'
+        log_level = logging.DEBUG
     elif e == 'testing':
         cfg_name = 'config.TestingConfig'
+        log_filename = 'testing.log'
+        log_level = logging.DEBUG
     else: # production as default
         cfg_name = 'config.ProductionConfig'
+        log_filename = 'production.log'
+        log_level = logging.WARNING
+
+    # add logging handler
+    rotating_file_handler = RotatingFileHandler(
+        os.path.join(app.instance_path, log_filename),
+        maxBytes=20000000,
+        backupCount=10,
+    )
+    # add handler to logger
+    for logger in (app.logger):
+        logger.addHandler(rotating_file_handler)
+        logger.setLevel(log_level)
+
     cfg = import_string(cfg_name)()
     app.config.from_object(cfg)
 
