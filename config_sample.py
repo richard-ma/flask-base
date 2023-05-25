80c0f898a00b7572aca7de5ea9a113295454df9f
Richard Ma <richard.ma.19850509@gmail.com>
2023-05-25 23:39:03 +0800

    add to str for Config class

--- config_sample.py
+++ config_sample.py
@@ -1,6 +1,9 @@
 class Config:
     TESTING = False
 
+    def __str__(self):
+        return "/".join([k+": "+v for k, v in vars(self.__class__).items() if not k.startswith('_')])
+
 class ProductionConfig(Config):
     DATABASE_URI = ''
 
