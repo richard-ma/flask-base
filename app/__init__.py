fcae4c82ff4a99f508bed4b1643fa360e67eef74
Richard Ma <richard.ma.19850509@gmail.com>
2023-05-25 22:34:52 +0800

    fix #8

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from werkzeug.utils import import_string
 
 
 def create_app():
@@ -10,6 +11,19 @@ def create_app():
         os.makedirs(app.instance_path)
     except OSError:
         pass # instance folder exists
+
+    # get flask environment variable
+    e = os.environ.get('FLASK_ENV', 'production') # default flask environment is production
+
+    # load configration
+    if e == 'dev':
+        cfg_name = 'config.DevelopmentConfig'
+    elif e == 'testing':
+        cfg_name = 'config.TestingConfig'
+    else: # production as default
+        cfg_name = 'config.ProductionConfig'
+    cfg = import_string(cfg_name)()
+    app.config.from_object(cfg)
 
     @app.route("/")
     def index():
