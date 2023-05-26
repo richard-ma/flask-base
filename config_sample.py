d0de28591f280cf43d63d53a613b3803834a9981
Richard Ma <richard.ma.19850509@gmail.com>
2023-05-26 09:48:32 +0800

    add some logging configurations

--- config_sample.py
+++ config_sample.py
@@ -1,5 +1,7 @@
 class Config:
     TESTING = False
+    LOGGING_FILE_MAX_BYTES = 1024 * 1024
+    LOGGING_FILE_BACKUP_COUNT = 10
 
     def __str__(self):
         return "/".join([k+": "+v for k, v in vars(self.__class__).items() if not k.startswith('_')])
